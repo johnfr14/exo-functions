52c854733b5766444e17d20596978c469f8465f3
johnfr14 <jonathan.alyra@gmail.com>
2021-03-03 19:13:36 +0100

    coucou

--- calc.js
+++ calc.js
@@ -1,3 +1,32 @@
 const add = (a, b) => {
   return a + b
 }
+
+const sub = (a, b) => {
+  return a - b
+}
+
+const mul = (a, b) => {
+  return a * b
+}
+
+const div = (a, b) => {
+  return a / b
+}
+
+const calc = (operande, nb1, nb2) => {
+  switch (operande){
+    case '+':
+      return console.log(add(nb1,nb2))
+    case '-':
+      return console.log(sub(nb1,nb2))
+    case '*':
+      return console.log(mul(nb1,nb2))
+    case '/':
+      return console.log(div(nb1,nb2))
+    default:
+      return console.log('Veuillez choisir un opérande entre [+, -, *, /] ')
+  }
+}
+
+console.log(calc('p',2,5))
---
+++ countWhells.js
@@ -0,0 +1,5 @@
+const countWheels = (nb_monocycle, nb_moto, nb_voiture, nb_limousine) => {
+  return nb_monocycle + nb_moto * 2 + nb_voiture * 4 + nb_limousine * 6  
+}
+
+console.log(countWheels(4,2,2,2))
